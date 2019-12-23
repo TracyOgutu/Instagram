4b83f20def7a3511dde51452e44a41648a75f17c
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-24 00:11:00 +0300

    add new image and new profile urls

--- insta/urls.py
+++ insta/urls.py
@@ -7,6 +7,8 @@ urlpatterns=[
     url('^$',views.welcome,name='welcome'),
     url(r'^search/',views.search_category,name='search_category'),
     url(r'^photo/(\d+)',views.single_photo,name='photo'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^new/profile$', views.new_profile, name='new-profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

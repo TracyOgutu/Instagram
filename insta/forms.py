ddab5033b84324b17b4b5dbbdb724bc7ae2bafd0
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-24 02:42:24 +0300

    add newcomment form

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile,Comments
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
@@ -19,3 +19,9 @@ class NewProfileForm(forms.ModelForm):
         widgets={
             'tags':forms.CheckboxSelectMultiple(),
         }
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        exclude=['editor']
+      
+       

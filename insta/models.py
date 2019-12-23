feb8921f43771e2a3ff75b4be34a9083408bc864
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-24 00:11:32 +0300

    add editor as a foreign key

--- insta/models.py
+++ insta/models.py
@@ -1,17 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo=models.ImageField(upload_to='images/')
+    profile_photo=models.ImageField(upload_to='images/',blank=True)
     bio=models.CharField(max_length=100)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.bio
+
+    @classmethod
+    def get_profile(cls):
+        profile=cls.objects.all()
+        return profile
+
+    
 class Image(models.Model):
-    image=models.ImageField(upload_to='images/')
+    image=models.ImageField(upload_to='images/',blank=True)
     image_name=models.CharField(max_length=30)
     image_caption=models.CharField(max_length=100)
-    profile=models.ForeignKey(Profile)
-    likes=models.IntegerField()
-    comments=models.CharField(max_length=100)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile,null=True)
+    likes=models.IntegerField(null=True)
+    comments=models.CharField(max_length=100,null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

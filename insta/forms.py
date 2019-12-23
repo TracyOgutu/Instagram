dea33f59c836f3f0388468bc8e82577dac197823
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-24 00:11:59 +0300

    add NewIMageForm and NewProfileForm

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from .models import Image,Profile
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['editor', 'pub_date','profile','likes','comments']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['editor']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }

46bd42bc52439ed50d6aca86fc0f26d3284822a3
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-03 07:37:59 +0300

    change email subject

--- insta/email.py
+++ insta/email.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     #creating a message subject and sender
-    subject='Welcome to Instagram'
+    subject='Welcome to Instatonne'
     sender='gwan.watchlist@gmail.com'
 
     #passing in the context variables

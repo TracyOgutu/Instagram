7aba54ae09c87049e4664f821e817154bdbd8878
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-09 12:52:23 +0300

    add docstrings

--- insta/views.py
+++ insta/views.py
@@ -11,6 +11,9 @@ from django.contrib import messages
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
+    '''
+    View for the main homepage.It retrieves authenticated user posts and posts for other users stories
+    '''
     everyoneimages=Image.get_images()
     comment=Comments.get_comments()
     users = User.objects.all()
@@ -38,6 +41,9 @@ def welcome(request):
     return render(request,'index.html',{"everyone":everyoneimages,"images":logged_in_user_posts,"letterForm":form,"profile":profile,"comment":comment,"users":users})
 
 def search_category(request):
+    '''
+    This method searches for an image by using the name of the image
+    '''
     if 'category' in request.GET and request.GET["category"]:
         search_term=request.GET.get("category")
         searched_categories=Image.search_by_category(search_term)
@@ -50,6 +56,9 @@ def search_category(request):
 
 @login_required(login_url='/accounts/login/')
 def single_photo(request,photo_id):
+    '''
+    This method displays a single photo and its details such as comments, date posted and caption
+    '''
 
     if request.method=='POST':
 
@@ -93,6 +102,9 @@ def new_image(request):
 
 @login_required(login_url='/accounts/login/')
 def new_profile(request):
+    '''
+    Used for creating a new profile for the user. It includes a profile photo and a bio
+    '''
     current_user = request.user
     if request.method == 'POST':
         form = NewProfileForm(request.POST, request.FILES)
@@ -107,6 +119,9 @@ def new_profile(request):
     return render(request, 'new_profile.html', {"form": form})
 
 def makecomment(request):
+    '''
+    View for making a comment 
+    '''
     current_user = request.user
     if request.method == 'POST':
         form = NewCommentForm(request.POST, request.FILES)
@@ -121,22 +136,34 @@ def makecomment(request):
     return render(request, 'comment.html', {"form": form})
    
 def delete_post(request,post_id=None):
+    '''
+    View for deleting a post by the user
+    '''
     post_to_delete=get_object_or_404(Image,id=post_id)
     post_to_delete.delete()
     return redirect('welcome')
 
 def like_a_post(request):
+    '''
+    View for liking a post
+    '''
     post = get_object_or_404(Image,id=request.POST.get('post_id'))
     post.likes.add(request.user)
     return redirect('welcome')
 
 def follow(request):
+    '''
+    View for following a user
+    '''
     post = get_object_or_404(Image,id=request.POST.get('post_id'))
     post.followers.add(request.user)
     return redirect('welcome')
 
 @login_required(login_url='/accounts/login/')
 def updateprofile(request):
+    '''
+    View for editing the profile 
+    '''
     if request.method=='POST':
         profileform=UpdateProfileForm(request.POST,request.FILES,instance=request.user)
         if profileform.is_valid():
@@ -154,6 +181,9 @@ def updateprofile(request):
 
 @login_required(login_url='/accounts/login/')
 def display_profile(request,user_id):
+    '''
+    View for displaying the profile for a single user
+    '''
     try:
         single_profile=Profile.single_profile(user_id)              
         image_posted=Image.user_images(user_id)
